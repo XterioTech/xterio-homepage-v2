d23dd716d9a78b44bd40049f2d4ffef7f27a2076
Yvette Leppard <yvette@superrb.com>
2024-09-24 14:31:11 +0100

    add button to hero banner

--- src/slices/HeroBanner/index.tsx
+++ src/slices/HeroBanner/index.tsx
@@ -1,5 +1,6 @@
 import { Content } from '@prismicio/client'
 import { SliceComponentProps } from '@prismicio/react'
+import Button, {ButtonVariant} from "@/components/button";
 
 /**
  * Props for `HeroBanner`.
@@ -11,6 +12,10 @@ export type HeroBannerProps = SliceComponentProps<Content.HeroBannerSlice>
  */
 const HeroBanner = ({ slice }: HeroBannerProps): JSX.Element => {
   const {
+    button_1_url,
+    button_1_text,
+    button_2_url,
+    button_2_text,
     theme
   } = slice.primary
 
@@ -26,6 +31,18 @@ const HeroBanner = ({ slice }: HeroBannerProps): JSX.Element => {
     >
       Placeholder component for hero_banner (variation: {slice.variation})
       Slices
+      <Button
+        href={button_1_url}
+        label={button_1_text}
+        variants={[ButtonVariant.black, ButtonVariant.round]}
+        className="hero-banner__button"
+      />
+      <Button
+        href={button_2_url}
+        label={button_2_text}
+        variants={[ButtonVariant.white, ButtonVariant.round]}
+        className="hero-banner__button"
+      />
     </section>
   )
 }
