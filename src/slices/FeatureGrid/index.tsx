7e017b4f23606b19987dfa39259728e969e10951
Yvette Leppard <yvette@superrb.com>
2024-09-25 14:19:56 +0100

    feature grid styling and markup

--- src/slices/FeatureGrid/index.tsx
+++ src/slices/FeatureGrid/index.tsx
@@ -1,5 +1,5 @@
 import { Content } from '@prismicio/client'
-import { SliceComponentProps } from '@prismicio/react'
+import { PrismicRichText, SliceComponentProps } from '@prismicio/react'
 
 /**
  * Props for `FeatureGrid`.
@@ -17,8 +17,16 @@ const FeatureGrid = ({ slice }: FeatureGridProps): JSX.Element => {
       data-slice-variation={slice.variation}
       data-slice-backgroundcolour={"light"}
     >
-      Placeholder component for feature_grid (variation: {slice.variation})
-      Slices
+      <div className="feature-grid__container">
+        {slice.primary.block.map(({ title, width, text }, index) => (
+          <div className="feature-grid__block" data-block-width={width.toLowerCase()
+            .split(' ')
+            .join('')} key={index}>
+            <h3 className="feature-grid__title">{title}</h3>
+            <div className="feature-grid__text"><PrismicRichText field={text} /></div>
+          </div>
+        ))}
+      </div>
     </section>
   )
 }
