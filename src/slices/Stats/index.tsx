5a626969702ded4e739896294587bb92bbae0f69
Yvette Leppard <yvette@superrb.com>
2024-09-26 11:02:19 +0100

    stats styling and markup

--- src/slices/Stats/index.tsx
+++ src/slices/Stats/index.tsx
@@ -1,5 +1,6 @@
-import { Content } from '@prismicio/client'
-import { SliceComponentProps } from '@prismicio/react'
+import {Content} from '@prismicio/client'
+import { PrismicRichText, SliceComponentProps } from '@prismicio/react'
+import TextLink from "@/components/text-link";
 
 /**
  * Props for `Stats`.
@@ -10,6 +11,11 @@ export type StatsProps = SliceComponentProps<Content.StatsSlice>
  * Component for "Stats" Slices.
  */
 const Stats = ({ slice }: StatsProps): JSX.Element => {
+  const {
+    url,
+    link_text
+  } = slice.primary
+
   return (
     <section
       className="stats"
@@ -17,7 +23,25 @@ const Stats = ({ slice }: StatsProps): JSX.Element => {
       data-slice-variation={slice.variation}
       data-slice-backgroundcolour={"light"}
     >
-      Placeholder component for stats (variation: {slice.variation}) Slices
+      <div className="stats__container">
+        <div className="stats__blocks">
+          {slice.primary.stats.map(({ title, text }, index) => (
+            <div className="stats__block" key={index}>
+              <h3 className="stats__title">{title}</h3>
+              <div className="stats__text"><PrismicRichText field={text} /></div>
+            </div>
+          ))}
+        </div>
+        <div className="stats__button">
+          {url && link_text && (
+            <TextLink
+              href={url}
+              label={link_text}
+              className="stats__text-link"
+            />
+          )}
+        </div>
+      </div>
     </section>
   )
 }
