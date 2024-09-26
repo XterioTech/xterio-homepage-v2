6fbf5800d08eb637fb29f405f2adefdd92a7f34e
Yvette Leppard <yvette@superrb.com>
2024-09-26 13:48:36 +0100

    parallax image section styling and markup

--- src/slices/ParallaxImage/index.tsx
+++ src/slices/ParallaxImage/index.tsx
@@ -1,5 +1,6 @@
 import { Content } from '@prismicio/client'
 import { SliceComponentProps } from '@prismicio/react'
+import ParallaxImage from '@/components/parallaxing-image'
 
 /**
  * Props for `Image`.
@@ -10,14 +11,20 @@ export type ImageProps = SliceComponentProps<Content.ImageSlice>
  * Component for "Image" Slices.
  */
 const Image = ({ slice }: ImageProps): JSX.Element => {
+  const { image } = slice.primary
+
   return (
     <section
-      className="parallax-image"
+      className="parallax-image-section"
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       data-slice-backgroundcolour={"image"}
     >
-      Placeholder component for image (variation: {slice.variation}) Slices
+      <ParallaxImage
+        image={image}
+        className="parallax-image-section__image objFit"
+        sizes="(min-width: 63.75em) 48vw, 95vw"
+      />
     </section>
   )
 }
