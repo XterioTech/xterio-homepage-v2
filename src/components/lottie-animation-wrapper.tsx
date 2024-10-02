89f8a2055f731f2f64299d14522048d4f8dd5ef5
James Dinsdale <hi@molovo.co>
2024-10-02 17:16:16 +0100

    Tweak animation quality and rendering to reduce performance issues

--- src/components/lottie-animation-wrapper.tsx
+++ src/components/lottie-animation-wrapper.tsx
@@ -1,9 +1,21 @@
 'use client'
 
+import { useIsInViewport } from '@superrb/react-addons/hooks'
 import Lottie, { LottieProps } from 'react-lottie-player'
+import LottiePlayer from 'lottie-web'
+
+LottiePlayer.setQuality(2)
 
 const LottieAnimationWrapper = (props: LottieProps) => {
-  return <Lottie {...props} />
+  const { isInViewport, setRef } = useIsInViewport()
+
+  props.play = isInViewport
+
+  return (
+    <div className="lottie-animation" ref={setRef}>
+      <Lottie renderer="svg" {...props} />
+    </div>
+  )
 }
 
 export default LottieAnimationWrapper
