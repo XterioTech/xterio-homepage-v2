ffda2451a3cb74e7af961e223717d0e138df2836
Yvette Leppard <yvette@superrb.com>
2024-10-02 10:49:54 +0100

    footer - TODO contact link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,7 @@
 import Logo from './logo'
 import { PrismicNextLink } from '@prismicio/next'
 import dynamic from 'next/dynamic'
+import Button, {ButtonVariant} from "@/components/button";
 
 const Navigation = dynamic(() => import('./navigation'))
 const SocialIcons = dynamic(() => import('./social-icons'))
@@ -11,16 +12,28 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer__container">
         <div className="footer__columns">
-          <PrismicNextLink href="/" className="footer__logo">
-            <Logo />
-          </PrismicNextLink>
+          <div className="footer__col">
+            <PrismicNextLink href="/" className="footer__logo">
+              <Logo />
+            </PrismicNextLink>
+            <div className="footer__contact">
+              <h3 className="footer__contact-title">YOUR JOURNEY STARTS HERE</h3>
+              <Button
+                href=""
+                label="Get in touch"
+                variants={[ButtonVariant.white]}
+                className="footer__contact-button"
+              />
+            </div>
+          </div>
+
           <div className="footer__menus">
             <Navigation className="footer-nav" name={'footer-menu-one'} />
             <Navigation className="footer-nav" name={'footer-menu-two'} />
           </div>
         </div>
         <div className="footer__row">
-          <div className="footer__info">© Copyright 2024 - Xterio Limited. All rights reserved. <a href="https://www.superrb.com" target="_blank" rel="noopener nofollow" title="Website by Superrb">Website by Superrb</a></div>
+          <p className="footer__info">© Copyright 2024 - Xterio Limited. All rights reserved. <a href="https://www.superrb.com" target="_blank" rel="noopener nofollow" title="Website by Superrb">Website by Superrb</a></p>
           <SocialIcons className="footer__social" />
         </div>
       </div>
