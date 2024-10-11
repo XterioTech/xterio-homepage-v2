2f48bccf23db404e50a04cae356816f462f97ee5
James Dinsdale <hi@molovo.co>
2024-10-11 12:44:39 +0100

    Remove unused pages

--- src/prismicio.ts
+++ src/prismicio.ts
@@ -1,12 +1,12 @@
-import * as prismic from "@prismicio/client";
-import * as prismicNext from "@prismicio/next";
-import config from "../slicemachine.config.json";
+import * as prismic from '@prismicio/client'
+import * as prismicNext from '@prismicio/next'
+import config from '../slicemachine.config.json'
 
 /**
  * The project's Prismic repository name.
  */
 export const repositoryName =
-  process.env.NEXT_PUBLIC_PRISMIC_ENVIRONMENT || config.repositoryName;
+  process.env.NEXT_PUBLIC_PRISMIC_ENVIRONMENT || config.repositoryName
 
 /**
  * A list of Route Resolver objects that define how a document's `url` field is resolved.
@@ -14,28 +14,16 @@ export const repositoryName =
  * {@link https://prismic.io/docs/route-resolver#route-resolver}
  */
 // TODO: Update the routes array to match your project's route structure.
-const routes: prismic.ClientConfig["routes"] = [
+const routes: prismic.ClientConfig['routes'] = [
   {
-  	type: "homepage",
-  	path: "/",
+    type: 'homepage',
+    path: '/',
   },
   {
     type: 'legal_page',
     path: '/legal/:uid',
   },
-  {
-    type: 'buy_page',
-    path: '/buy',
-  },
-  {
-    type: 'build_page',
-    path: '/build',
-  },
-  {
-    type: 'ecosystem_page',
-    path: '/ecosystem',
-  }
-];
+]
 
 /**
  * Creates a Prismic client for the project's repository. The client is used to
@@ -47,17 +35,17 @@ export const createClient = (config: prismicNext.CreateClientConfig = {}) => {
   const client = prismic.createClient(repositoryName, {
     routes,
     fetchOptions:
-      process.env.NODE_ENV === "production"
-        ? { next: { tags: ["prismic"] }, cache: "force-cache" }
+      process.env.NODE_ENV === 'production'
+        ? { next: { tags: ['prismic'] }, cache: 'force-cache' }
         : { next: { revalidate: 5 } },
     ...config,
-  });
+  })
 
   prismicNext.enableAutoPreviews({
     client,
     previewData: config.previewData,
     req: config.req,
-  });
+  })
 
-  return client;
-};
+  return client
+}
