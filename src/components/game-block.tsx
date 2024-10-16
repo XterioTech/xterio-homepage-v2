8c8d79f9512d90d04596601d05e21957fe151626
Yvette Leppard <yvette@superrb.com>
2024-10-16 09:05:33 +0100

    add image zoom and link entire game block - fixes #20

--- src/components/game-block.tsx
+++ src/components/game-block.tsx
@@ -1,4 +1,4 @@
-import { Image } from '@superrb/next-addons/components'
+import {Image, LinkBase} from '@superrb/next-addons/components'
 import { ImageProps } from '@superrb/next-addons/components/image'
 import TextLink from '@/components/text-link'
 import { KeyTextField, LinkField } from '@prismicio/client'
@@ -16,21 +16,17 @@ const GameBlock = ({
   buttonUrl,
 }: Props) => {
   return (
-    <div className="game-block">
+    <LinkBase href={buttonUrl} className="game-block">
       <div className="game-block__type">{blockType}</div>
       <Image
         image={image}
         className="game-block__image objFit"
         sizes="(min-width: 63.75em) 33.333vw, 100vw"
       />
-      {buttonUrl && buttonText && (
-        <TextLink
-          href={buttonUrl}
-          label={buttonText}
-          className="game-block__text-link"
-        />
-      )}
-    </div>
+      <span className="game-block__text-link">
+        {buttonText}
+      </span>
+    </LinkBase>
   )
 }
 
