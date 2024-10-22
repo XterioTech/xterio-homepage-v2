25d69c52934d2cd966b59c85ea94fa3e745874c1
Yvette Leppard <yvette@superrb.com>
2024-10-22 09:20:39 +0100

    style image with caption slice

--- src/slices/ImageWithCaption/index.tsx
+++ src/slices/ImageWithCaption/index.tsx
@@ -1,5 +1,6 @@
 import { Content } from '@prismicio/client'
 import { SliceComponentProps } from '@prismicio/react'
+import {Image} from "@superrb/next-addons/components";
 
 /**
  * Props for `ImageWithCaption`.
@@ -11,15 +12,39 @@ export type ImageWithCaptionProps =
  * Component for "ImageWithCaption" Slices.
  */
 const ImageWithCaption = ({ slice }: ImageWithCaptionProps): JSX.Element => {
+  const {
+    image,
+    caption,
+    width
+  } = slice.primary
+
   return (
     <section
       className="image-with-caption"
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      data-slice-backgroundcolour={"dark"}
+      data-slice-backgroundcolour={"light"}
+      data-slice-width={width.replace(/[\W_]+/g," ")
+        .toLowerCase()
+        .split(' ')
+        .join('')}
     >
-      Placeholder component for image_with_caption (variation: {slice.variation}
-      ) Slices
+      <div className="image-with-caption__container">
+        <div className="image-with-caption__inner">
+          <Image
+            image={image}
+            className="image-with-caption__image"
+            sizes=""
+          />
+          {caption && (
+            <div className="image-with-caption__footer">
+              <div className="image-with-caption__footer-container">
+                <p className="image-with-caption__caption">{caption}</p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </section>
   )
 }
