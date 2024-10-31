a5766d61fec2bd11ec3e41612ab4e55e8e5803c5
James Dinsdale <hi@molovo.co>
2024-10-31 10:33:22 +0000

    Remove async/await from client component

---
+++ src/components/latest-news-header.tsx
@@ -0,0 +1,21 @@
+import { animator } from '@superrb/react-addons/utils'
+import { LegacyRef, ReactNode } from 'react'
+
+const LatestNewsHeader = ({
+  title,
+  socialIcons,
+}: {
+  title: string
+  socialIcons: ReactNode
+}) => (
+  <header className="latest-news__header news-header">
+    <h3
+      className="news-header__title"
+      ref={animator as LegacyRef<HTMLHeadingElement>}
+    >
+      {title}
+    </h3>
+  </header>
+)
+
+export default LatestNewsHeader
--- src/components/latest-news.tsx
+++ src/components/latest-news.tsx
@@ -1,30 +1,30 @@
-'use client'
-
 import { createClient } from '@/prismicio'
-import dynamic from "next/dynamic";
-import {Image, LinkBase} from "@superrb/next-addons/components";
-import Author from "@/components/author";
-const SocialIcons = dynamic(() => import('./social-icons'))
-import {animator} from "@superrb/react-addons/utils";
-import {LegacyRef, MutableRefObject, useEffect, useState} from "react";
+import dynamic from 'next/dynamic'
+import { Image, LinkBase } from '@superrb/next-addons/components'
+import Author from '@/components/author'
+import LatestNewsHeader from './latest-news-header'
+import SocialIcons from './social-icons'
 
 const LatestNews = async () => {
   const client = createClient()
   const newsContent = await client.getSingle('latest_news')
 
-  const {title, news_item} = newsContent?.data
+  const { title, news_item } = newsContent?.data
+
+  const socialIcons = <SocialIcons className="news-header__social" />
 
   return (
-    <div className="latest-news" data-slice-backgroundcolour={"dark"}>
+    <div className="latest-news" data-slice-backgroundcolour={'dark'}>
       <div className="latest-news__container">
-        <header className="latest-news__header news-header">
-          <h3 className="news-header__title" ref={animator as LegacyRef<HTMLHeadingElement>}>{title}</h3>
-          <SocialIcons className="news-header__social" />
-        </header>
+        <LatestNewsHeader title={title as string} socialIcons={socialIcons} />
 
         <div className="latest-news__blocks">
-          {news_item.map(({ image, category, title, author, link}, index) => (
-            <LinkBase href={link} key={index} className="latest-news__block news-block">
+          {news_item.map(({ image, category, title, author, link }, index) => (
+            <LinkBase
+              href={link}
+              key={index}
+              className="latest-news__block news-block"
+            >
               <Image
                 image={image}
                 className="news-block__image"
