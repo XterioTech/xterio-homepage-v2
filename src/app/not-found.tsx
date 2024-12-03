964d6377712fe9e799f2ef410bf53e1296ebc2e3
James Dinsdale <hi@molovo.co>
2024-12-03 12:12:50 +0000

    Load 404 page from default locale

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,35 +5,12 @@ import Scripts from '@/components/scripts'
 import SocialIcons from '@/components/social-icons'
 import { createClient } from '@/prismicio'
 import { components } from '@/slices'
-import { setTranslationLanguage } from '@/utils/translate'
-import { Language } from '@prismicio/client'
 import { SliceZone } from '@prismicio/react'
 import { CookieBanner } from '@superrb/react-addons/components'
-import { headers } from 'next/headers'
 
 export default async function Page() {
   const client = createClient()
-
-  const pathname = headers().get('x-pathname') as string
-  const repository = await client.getRepository()
-
-  const locales = repository.languages.map((lang: Language) => lang.id)
-  const defaultLocale = locales[0]
-
-  const pathnameIsMissingLocale = locales.every(
-    (locale) =>
-      !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`,
-  )
-
-  let locale = defaultLocale
-  // Redirect to default locale if there is no supported locale prefix
-  if (!pathnameIsMissingLocale) {
-    locale = pathname.split('/')[1]
-  }
-
-  const page = await client.getByUID('error_page', '404', {
-    lang: locale,
-  })
+  const page = await client.getByUID('error_page', '404')
 
   const navigation = (
     <>
@@ -42,7 +19,6 @@ export default async function Page() {
     </>
   )
 
-  setTranslationLanguage(locale)
   const socialIcons = <SocialIcons className="header__social" />
 
   return (
